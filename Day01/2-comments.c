e01fddf2ab210f3e1cd3fd99b4741f6da9911494
YourAKShaw <hitme@yourakshaw.com>
2024-10-22 19:40:50 +0530

    feat(script): :sparkles: add first 2 scripts for Day01

---
+++ Day01/2-comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // This is a single line comment.
+  /*
+    This is a multi line comment.
+  */
+ /**
+  * This is a documentation comment.
+  */
+
+  printf("C has 3 types of comments.\n");
+  return 0;
+}
